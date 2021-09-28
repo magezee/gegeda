edb1e2076a019c55ca14f5b7786e52969c566cc6
gegeda <xieqi@bilibili.com>
2021-09-28 18:33:24 +0800

    fix: change host config

--- scripts/constant.js
+++ scripts/constant.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 const PROJECT_PATH = path.resolve(__dirname, '../')   // 项目根路径
 
-const SERVER_HOST = '127.0.0.1'
-const SERVER_PORT = 3000
+const SERVER_HOST = '0.0.0.0'
+const SERVER_PORT = 80
 
 module.exports =  {
   PROJECT_PATH,
